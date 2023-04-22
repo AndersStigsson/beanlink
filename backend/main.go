153457e9a6f7e964bb199827b42fb1c4a8cf9823
Anders Stigsson <anders@pixelstore.se>
2023-04-22 08:10:54 +0200

    Return same short link if already added

--- backend/main.go
+++ backend/main.go
@@ -76,10 +76,11 @@ func (s *Server) addNewLink(w http.ResponseWriter, r *http.Request) {
 
 	var exists string
 
-	err := s.db.Get(&exists, "SELECT id_link FROM links WHERE return_link = ?", link)
-	if err != nil {
-		returnLink := fmt.Sprintf("https://beanl.ink/l/%s", exists)
-		json.NewEncoder(w).Encode(Link{Link: returnLink})
+	s.db.Get(&exists, "SELECT id_link FROM links WHERE return_link = ?", link.Link)
+	if exists != "" {
+		existingLink := fmt.Sprintf("https://beanl.ink/l/%s", exists)
+		json.NewEncoder(w).Encode(Link{Link: existingLink})
+		return
 	}
 
 	id_link := String(10)
